ccb98b709c17ed95dce4ee476d74acbc5cca6b23
achewink <achewink@gmail.com>
2015-12-03 19:10:34 +0800

    minor UI improved
    minor UI improved

--- _site/assets/js/main.js
+++ _site/assets/js/main.js
@@ -1,14 +1,5 @@
 $(document).ready(function() {
 
-
-  $('.close-btn').click(function() {
-    // $('.hambg-list').removeClass('come-on').addClass("go-off");
-  });
-
-  $('.open-button').click(function() {
-    // $('.hambg-list').removeClass('go-off').addClass('come-on');
-  });
-
   // Click hamburger icon to toggle hidden list for mobile users
   $('.open-button').on('click', function(event) {
     $('.hambg-list').toggleClass('come-on');
@@ -16,11 +7,8 @@ $(document).ready(function() {
   })
 
   document.getElementById("open-button").addEventListener('click', function(e) {
-    console.log('hihi');
     document.getElementById('burgericon').classList.toggle('open');
   });
-
-  console.log('fdsifhsidufhlisuh');
 
   //
   // -ms-transform: rotate(9deg); /* IE 9 */
