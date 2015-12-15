6d9a12fb4fa8d89d82905ce81d1305f5aa35227e
achewink <achewink@gmail.com>
2015-12-15 18:52:43 +0800

    content updated
    content updated

--- _site/assets/js/main.js
+++ _site/assets/js/main.js
@@ -6,13 +6,12 @@ $(document).ready(function() {
     $('.hambg-list').toggleClass('go-off');
   })
 
+  $('.open-button').on('click', function(event) {
+    $('body').toggleClass('scroll-locked');
+  })
+
   document.getElementById("open-button").addEventListener('click', function(e) {
     document.getElementById('burgericon').classList.toggle('open');
   });
-
-  //
-  // -ms-transform: rotate(9deg); /* IE 9 */
-  //     -webkit-transform: rotate(9deg); /* Chrome, Safari, Opera */
-  //     transform: rotate(9deg);
 
 });
